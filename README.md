eda6d44ca4fa3e18000f5fce031d397aae59b9dd
Lucas Matos <lucas.suporte.tec@outlook.com>
2022-10-26 00:02:33 -0400

    ädicionando javascritp da calculadora

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Project Calculator Apps Ideas
 https://github.com/florinpop17/app-ideas
 
 https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Calculator-App.md
+
+#Video para explicação
+
+https://www.youtube.com/watch?v=KCfaPZ2t2yA&ab_channel=MatheusBattisti-HoradeCodar
--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
-const previusOperationsText = document.querySelector("#previus-operation");
+const previousOperationsText = document.querySelector("#previus-operation");
 const currentOperationText = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
 
-class Calculator {}
+class Calculator {
+  constructor(previousOperationsText, currentOperationText) {
+    this.previousOperationsText = previousOperationsText;
+    this.currentOperationText = currentOperationText;
+    this.currentOperation = "";
+  }
+
+  //add digit to calculator screen
+  addDigit(digit) {
+    // check if current operation already has a dot
+    if (digit === "." && this.currentOperationText.innerText.includes(".")) {
+      return;
+    }
+    this.currentOperation = digit;
+    this.updateScreen();
+  }
+
+  updateScreen() {
+    this.currentOperationText.innerText += this.currentOperation;
+  }
+}
+
+const calc = new Calculator(previousOperationsText, currentOperationText);
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const value = e.target.innerText;
 
     if (+value >= 0 || value === ".") {
-      console.log(value);
+      calc.addDigit(value);
     } else {
       console.log("Op: " + value);
     }
