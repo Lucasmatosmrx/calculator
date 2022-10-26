913e04d965b686599af696fadfc88036ca5f3d18
Lucas Matos <lucas.suporte.tec@outlook.com>
2022-10-25 23:25:35 -0400

    add calculado em html com css

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const previusOperationsText = document.querySelector("#previus-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator {}
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const value = e.target.innerText;
+
+    if (+value >= 0 || value === ".") {
+      console.log(value);
+    } else {
+      console.log("Op: " + value);
+    }
+  });
+});
